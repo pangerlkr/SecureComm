5d9958d08ebefb540291e0bccc57d4468962e008
Panger Lkr <73515951+pangerlkr@users.noreply.github.com>
2026-01-15 10:20:08 +0530

    Update CORS origin for Socket.IO server

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://panger-chat.netlify.app"],
+    origin: ["http://localhost:5173", "https://securecomm.netlify.app"],
     methods: ["GET", "POST"]
   }
 });
